0c2cfa211f48e478344ff9affcbddf2cef1f4afd
Luca <luca.clerici@studenti.unitn.it>
2026-05-13 18:55:16 +0200

    implementazione middleware ruoli, si usa in cocomitanza del middleware auth, si importano entrambi i moduli, per route protette, (es: creazione di eventi), con autorizzaRuoli(''ruoloRichiesto''), si verifica se l'utente ha tale ruolo

--- backend/routes/segnalazioni.js
+++ backend/routes/segnalazioni.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.send("Route segnalazioni OK");
 });
 
+//testing middleware ruoli per segnalazioni
+import { auth } from "../middleware/auth.js";
+import { autorizzaRuoli } from "../middleware/ruoli.js";
+
+router.get("/solo-admin", auth, autorizzaRuoli("AMMINISTRATORE"), (req, res) => {
+  res.json({ messaggio: "Accesso consentito: sei admin" });
+});
+
 export default router;
