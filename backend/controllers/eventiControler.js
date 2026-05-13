1436f1036033d810f1491a125fa8ce014ba7d665
emanuelbertoldo02 <emanuelbertoldo02@gmail.com>
2026-05-13 14:54:55 +0000

    Aggiunta gestione errori in eventi

--- backend/controllers/eventiControler.js
+++ backend/controllers/eventiControler.js
@@ -1,24 +1,37 @@
 import { Evento } from "../models/schema.js";
 
 const getEventiByVia = async (idVia) => {
-    const eventi = await Evento.find({ via: idVia }).populate("via");
-    return eventi;
+    if (!idVia) {return { error: "ID della via mancante" };}
+    try {
+        const eventi = await Evento.find({ via: idVia }).populate("via");
+        return eventi;
+    } catch (error) {return { error: "Errore durante il recupero degli eventi " + error.message };}
 }
 
 const getEventoById = async (id) => {
-    const evento = await Evento.findById(id).populate("via");
-    return evento;
+    if (!id) {return { error: "ID dell'evento mancante" };}
+    try {
+        const evento = await Evento.findById(id).populate("via");
+        return evento;
+    } catch (error) {return { error: "Errore durante il recupero dell'evento " };}
 }
 
 const createEvento = async (evento) => {
-    const newEvento = new Evento(evento);
-    await newEvento.save();
-    return newEvento;
+    if (!evento) {return { error: "Dati dell'evento mancanti" };}
+    try {
+        const newEvento = new Evento(evento);
+        await newEvento.save();
+        return newEvento;
+    } catch (error) {return { error: "Errore durante la creazione dell'evento " + error.message };}
 }
 
 const updateEvento = async (id, evento) => {
-    const updatedEvento = await Evento.findByIdAndUpdate(id, evento, { new: true });
-    return updatedEvento;
+    if (!id) {return { error: "ID dell'evento mancante" };}
+    if (!evento) {return { error: "Dati dell'evento mancanti" };}
+    try {
+        const updatedEvento = await Evento.findByIdAndUpdate(id, evento, { new: true });
+        return updateEvento;
+    } catch (error) {return { error: "Errore durante l'aggiornamento dell'evento " + error.message };}
 }
 
 export { getEventiByVia, getEventoById, createEvento, updateEvento }
--- backend/routes/eventi.js
+++ backend/routes/eventi.js
@@ -8,31 +8,32 @@ router.get("/", (req, res) => {
 });
 
 //get eventi by via
-router.get("/via/:via", (req, res) => {
+router.get("/via/:via", async (req, res) => {
   const via = req.params.via;
-  const eventi = getEventiByVia(via);
+  const eventi = await getEventiByVia(via);
+  console.log(eventi);
   res.json(eventi);
 });
 
 //get eventi by id
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const evento = getEventoById(id);
+  const evento = await getEventoById(id);
   res.json(evento);
 });
 
 //create evento
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
   const evento = req.body;
-  const evento2 = createEvento(evento);
+  const evento2 = await createEvento(evento);
   res.json(evento2);
 });
 
 //update evento
-router.post("/update/:id", (req, res) => {
+router.post("/update/:id", async (req, res) => {
   const id = req.params.id;
   const evento = req.body;
-  const evento2 = updateEvento(id, evento);
+  const evento2 = await updateEvento(id, evento);
   res.json(evento2);
 });
 
