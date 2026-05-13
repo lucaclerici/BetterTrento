f7c9727e52dab69f41fc1c1d60e73c32230956c3
Luca <luca.clerici@studenti.unitn.it>
2026-05-13 12:37:38 +0200

    implementazione file (per il momento vuoti) nella cartelle

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import utentiRoutes from "./routes/utenti.js";
+import segnalazioniRoutes from "./routes/segnalazioni.js";
+import eventiRoutes from "./routes/eventi.js";
+import informazioniRoutes from "./routes/informazioni.js";
 
-// Middleware per leggere JSON
+const app = express();
 app.use(express.json());
 
 // Route di test
 app.get('/', (req, res) => {
   res.send('API BetterTrento attiva!');
 });
+
+// Connessione al DB
+mongoose.connect("mongodb+srv://user_betterTrento:BetterTrento1@cluster0.rgtitef.mongodb.net/")
+  .then(() => console.log("MongoDB connesso"))
+  .catch(err => console.error(err));
+
+
+// ROUTES
+app.use("/api/utenti", utentiRoutes);
+app.use("/api/segnalazioni", segnalazioniRoutes);
+app.use("/api/eventi", eventiRoutes);
+app.use("/api/informazioni", informazioniRoutes);
 
 // Avvio server
 app.listen(3000, () => {
