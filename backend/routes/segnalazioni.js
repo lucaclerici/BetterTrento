19f39c417d166fb0f806cc78052b978575d46d60
Luca <luca.clerici@studenti.unitn.it>
2026-05-14 17:19:14 +0200

    inserimento route in segnalazioni

--- backend/routes/segnalazioni.js
+++ backend/routes/segnalazioni.js
@@ -7,15 +7,16 @@ router.get("/", (req, res) => {
 });
 
 
+router.post("/", auth, creaSegnalazione);//per creare una segnalazione
 
+router.get("/mie", auth, mieSegnalazioni);//per vedere le segnalazioni create di un singolo utente
+router.get("/", auth, autorizzaRuoli("AMMINISTRATORE"), tutteSegnalazioni);//per vedere tutte le segnalazioni (solo admin)
+router.get("/:id", auth, dettaglioSegnalazione);//per vedere nel dettaglio una segnalazione
+router.get("/per-via/:idVia", auth, segnalazioniPerVia);//per vedere tutte le segnalazioni di una data via
 
+router.put("/:id/stato", auth, autorizzaRuoli("AMMINISTRATORE"), aggiornaStato);//per cambiare lo stato di una via (solo admin)
 
-//testing middleware ruoli per segnalazioni
-/*import { auth } from "../middleware/auth.js";
-import { autorizzaRuoli } from "../middleware/ruoli.js";
+router.delete("/:id", auth, autorizzaRuoli("AMMINISTRATORE"), eliminaSegnalazione);//per eliminare una segnalazione (solo admin)
 
-router.get("/solo-admin", auth, autorizzaRuoli("AMMINISTRATORE"), (req, res) => {
-  res.json({ messaggio: "Accesso consentito: sei admin" });
-});*/
 
 export default router;
