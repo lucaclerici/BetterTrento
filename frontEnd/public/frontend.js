dabf43f6a7e39fc12a3d2f7e014884893baed72e
Luca <luca.clerici@studenti.unitn.it>
2026-05-26 17:55:15 +0200

    implementazione base base della pagina del profilo dell'utente. Un Utente può vedere tale pagina con i dati del suo profilo, dopo che ha eseguito il login correttamente, e ha cliccato sul icona del profilo a DX nella navbar, è stato inoltre implementato un bottone di logout nella pagina profilo, che disconette l'utente e lo reinderizza alla homepage

--- frontEnd/public/frontend.js
+++ frontEnd/public/frontend.js
@@ -4,9 +4,16 @@ const loginArea = document.getElementById("login-area");
 
 if (isLogged === "true") {
     loginArea.innerHTML = `
-            <div class="user-box">
-                <img src="/frontEnd/image/immagine_ominoProfilo.jpg" class="user-icon">
-                <span class="user-greeting">Ciao</span>
-            </div>
-        `;
+        <div class="user-box" id="user-box">
+            <img src="/frontEnd/image/immagine_ominoProfilo.jpg" class="user-icon">
+            <span class="user-greeting">Ciao</span>
+        </div>
+    `;
 }
+
+document.addEventListener("click", (e) => {
+    const userBox = document.getElementById("user-box");
+    if (userBox && userBox.contains(e.target)) {
+        window.location.href = "/frontEnd/public/profilo.html";
+    }
+});
