6701762c4e8143ae29bcbc0ffd07ee2547c1d8bc
Luca <luca.clerici@studenti.unitn.it>
2026-06-02 17:49:56 +0200

    implementazione collegamento backend e frontend(molto basso), tutto funzionante

--- frontEnd/public/informazioni.js
+++ frontEnd/public/informazioni.js
@@ -1,40 +1,60 @@
 //CARICA GLI EVENTI PER LA VIA INSERITA -> se UTENTE LOGGATO allora si usa la VIA salvata in profilo
 async function caricaEventi(via = null) {
 
+    const container = document.getElementById("reports-feed-eventi");
+    const viaId = via;
+    const res = await fetch(`http://localhost:3000/api/eventi/via/${viaId}`);
+    const eventi = await res.json();
+
+    container.innerHTML = "";
+
+    if (!eventi || eventi.length === 0) {
+        return;
+    }
+
+    eventi.forEach(ev => {
+        const card = document.createElement("div");
+        card.classList.add("report-card");
+        card.style.minWidth = "250px";
+        card.style.maxWidth = "25%";
+        card.innerHTML = `
+            <div class="report-content">
+                <h3>Evento: ${ev.nome}</h3>
+                <p>${ev.descrizione}</p>
+                <p><strong>Data:</strong> ${new Date(ev.data).toLocaleDateString()}</p>
+                <p><strong>Via:</strong> ${ev.via?.strada || "N/D"}</p>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
 }
 
 
 //CARICA LE SEGNALAZIONI PER LA VIA INSERITA -> -> se UTENTE LOGGATO allora si usa la VIA salvata in profilo
 async function caricaSegnalazioni(via = null) {
-    const feed = document.getElementById("reports-feed");
-
-    const token = localStorage.getItem("token");
-    if(token){
-
-    }
-    const res = await fetch(`http://localhost:3000/api/segnalazioni/per-via/${idVia}`);
+    const feed = document.getElementById("reports-feed-segnalazioni");
+    const viaId = via;
+    const res = await fetch(`http://localhost:3000/api/segnalazioni/per-via/${viaId}`);
     const segnalazioni = await res.json();
 
     feed.innerHTML = "";
 
-    const ruolo = getUserRole();//prende il ruolo dalla funzione in frontend.js
-
     if (segnalazioni.length === 0) {
-        feed.innerHTML = "<p>Nessuna segnalazione trovata per questa via.</p>";
         return;
     }
 
     segnalazioni.forEach(seg => {
         const card = document.createElement("div");
         card.classList.add("report-card");
-
+        card.style.minWidth = "250px";
+        card.style.maxWidth = "25%";
         card.innerHTML = `
-            <h3>${seg.nome}</h3>
+            <h3>Segn: ${seg.nome}</h3>
             <p>${seg.descrizione}</p>
             <p><strong>Problema:</strong> ${seg.problema?.nome || "N/D"}</p>
             ${seg.immagini ? `<img src="${seg.immagini}" class="report-img" style="max-width: 20%;">` : ""}
         `;
-
         feed.appendChild(card);
     });
 }
@@ -63,16 +83,22 @@ async function controllaProfiloEVia() {
                     });
                     const r = await res.json();
                     const strada = r.strada;
-                    console.log(strada);
                     document.getElementById('via').value = strada;//riempio di base la barra di ricerca della via con la via dell'utente
-                    caricaSegnalazioni(viaId);
                     caricaEventi(viaId);
+                    caricaSegnalazioni(viaId);
                 }
             }
         } catch (error) { }
     }
 }
 
+document.getElementById("btn-cerca").addEventListener("click", () => {
+    const viaInput = document.getElementById("via");
+    const viaId = viaInput.dataset.viaId;
+
+    caricaEventi(viaId);
+    caricaSegnalazioni(viaId);
+});
 
 window.addEventListener('DOMContentLoaded', () => {
     controllaProfiloEVia();
