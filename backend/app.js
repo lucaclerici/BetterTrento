6d90e967fa27fe93886169f83185d08b74fa84c1
Luca <luca.clerici@studenti.unitn.it>
2026-06-04 20:27:53 +0200

    sistemazione ed eleminazione API 'informazioni' prevista ad inizio progetto

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import utentiRoutes from "./routes/utenti.js";
 import segnalazioniRoutes from "./routes/segnalazioni.js";
 import eventiRoutes from "./routes/eventi.js";
-import informazioniRoutes from "./routes/informazioni.js";
 import vieRoutes from "./routes/vie.js";
 import cors from "cors";
 import dotenv from "dotenv";//serve per leggere la chiave per i token e indirizzo del DB scritti nel file .env
@@ -31,7 +30,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/utenti", utentiRoutes);
 app.use("/api/segnalazioni", segnalazioniRoutes);
 app.use("/api/eventi", eventiRoutes);
-app.use("/api/informazioni", informazioniRoutes);
 app.use("/api/vie", vieRoutes);
 
 // Avvio server
--- backend/routes/informazioni.js
+++
@@ -1,9 +0,0 @@
-//lista di segnalazioni e eventi
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Route informazioni OK");
-});
-
-export default router;
