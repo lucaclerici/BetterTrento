09bd91f0de28cf8583497d91b2eeee80e3ce8647
Luca <luca.clerici@studenti.unitn.it>
2026-06-10 22:10:14 +0200

    fix link

--- frontEnd/public/frontend.js
+++ frontEnd/public/frontend.js
@@ -9,7 +9,8 @@ if (loginArea && isLogged === "true") {
     linkAccesso.href = "profilo.html";
     loginArea.innerHTML = `
         <div class="user-box" id="user-box">
-            <img src="../image/immagine_ominoProfilo.jpg" class="user-icon">
+            <img src="<img src="../image/immagine_ominoProfilo.jpg" class="user-icon">" class="user-icon">
+            
             <span class="user-greeting">Ciao</span>
         </div>
     `;
