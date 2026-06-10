0bb4f3d57accb3f2d5efc27c5beb84d9c8f5495b
Luca <luca.clerici@studenti.unitn.it>
2026-06-10 18:10:42 +0200

    testing

--- frontEnd/public/frontend.js
+++ frontEnd/public/frontend.js
@@ -9,7 +9,7 @@ if (loginArea && isLogged === "true") {
     linkAccesso.href = "profilo.html";
     loginArea.innerHTML = `
         <div class="user-box" id="user-box">
-            <img src="/frontEnd/image/immagine_ominoProfilo.jpg" class="user-icon">
+            <img src="/BetterTrento/frontEnd/image/immagine_ominoProfilo.jpg" class="user-icon">
             <span class="user-greeting">Ciao</span>
         </div>
     `;
@@ -18,7 +18,7 @@ if (loginArea && isLogged === "true") {
 document.addEventListener("click", (e) => {
     const userBox = document.getElementById("user-box");
     if (userBox && userBox.contains(e.target)) {
-        window.location.href = "/frontEnd/public/profilo.html";
+        window.location.href = "profilo.html";
     }
 });
 
